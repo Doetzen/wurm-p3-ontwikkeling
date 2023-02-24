c13cb09f2c08e4afdac0a5071dd1dc4432074b7a
Doetzen <Doutzenhoekstra06@gmail.com>
2023-02-24 16:05:25 +0100

    betere movement en klein beetje verder pipe minigame

--- wurm p3 ontwikkeling/Assets/Scripts/Movement/Movement.cs
+++ wurm p3 ontwikkeling/Assets/Scripts/Movement/Movement.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float hor, ver, speed;
+   
+    public float hor, ver, speed, sprint;
     public Vector3 movement, jump;
     public RaycastHit hit;
     public Rigidbody rb;
 
     private void Start()
     {
-        
+        speed = 5;
+        sprint = 10;
     }
 
     // Update is called once per frame
@@ -28,13 +30,13 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            speed = 10;
+            speed = sprint;
         }
-        else 
+        else
         {
             speed = 5;
         }
-
+       
         if(Physics.Raycast(transform.position, -transform.up, out hit, 1.5f))
         {
             if (hit.transform.tag == "jumpable")
