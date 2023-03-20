203c41a73e67e3470b552db8a1f9477129f1af81
Suus2005 <Susanwinkels34@gmail.com>
2023-03-20 15:32:49 +0100

    conversation system start
    klein beetje begin
    alvast een deel van de tekst erin

---
+++ wurm p3 ontwikkeling/Assets/Main/conversation system/Convo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Conversation", menuName = "ConversationMenu", order = 1)]
+
+
+public class Convo : ScriptableObject
+{
+    public string[] convoText;
+}
+
+
