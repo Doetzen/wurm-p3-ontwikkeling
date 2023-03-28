ba8c4c8321949a1f550c0df5f5d73e3516b98491
CerberusCR <124683059+CerberusCR@users.noreply.github.com>
2023-03-28 15:30:55 +0200

    wat met pause menu in eigen scene en 1 fakkel in main scene

--- wurm p3 ontwikkeling/Assets/Main/Scripts/Scripts Cayleigh/PauseMenu.cs
+++ wurm p3 ontwikkeling/Assets/Main/Scripts/Scripts Cayleigh/PauseMenu.cs
@@ -5,45 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour   
 {
-    public static bool Paused = false;
-    public GameObject PauseMenuCanvas;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
     {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Home(int sceneID)
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            if(Paused)
-            {
-                Play();
-            }
-            else
-            {
-                Stop();
-            }
-        }
-    }
-    void Stop()
-    {
-        PauseMenuCanvas.SetActive(true);
-            Time.timeScale = 0f;
-        Paused = true;
-    }
-
-    public void Play()
-    {
-        PauseMenuCanvas.SetActive(false);
         Time.timeScale = 1f;
-        Paused = false;
-    }
-
-    public void MainMenuButton()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(sceneID);
     }
 }
