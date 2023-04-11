aeeb2585d5451cec6e1979251615806e4f751296
Suus2005 <Susanwinkels34@gmail.com>
2023-04-11 18:13:46 +0200

    bezig met de sign text ding
    werkt half, geen errors

--- wurm p3 ontwikkeling/Assets/Main/Scripts/Scripts Suus/RaySign.cs
+++ wurm p3 ontwikkeling/Assets/Main/Scripts/Scripts Suus/RaySign.cs
@@ -2,35 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RaySign : MonoBehaviour
 {
     public RaycastHit hit;
-    public TMP_Text signText;
+    public TMP_Text pressE,signText;
+    public Image theSign;
     public Convo signs;
+    public bool seeTheSing;
 
 
+    public void Start()
+    {
+        pressE.gameObject.SetActive(false);
+        signText.gameObject.SetActive(false);
+        theSign.gameObject.SetActive(false);
+        seeTheSing= false;
+
+    }
 
     void Update()
     {
         if (Physics.Raycast(transform.position, transform.forward, out hit, 2))
         {
-            signText.gameObject.SetActive(true);
-            signText.text = ("Press E to read");
-
-            if (Input.GetKeyDown(KeyCode.E))
+            if (hit.collider.tag == "sign")
             {
-
-                if (hit.collider.tag == "sign")
+               pressE.gameObject.SetActive(true);
+               pressE.text = ("Press E to read");  
+                if (Input.GetKeyDown(KeyCode.E))
                 {
+                    if (seeTheSing == false)
+                    {
+                        theSign.gameObject.SetActive(true);
+                        seeTheSing = true;
+
+
+
+
+                    }
+                    else if (seeTheSing == true)
+                    { 
+                        theSign.gameObject.SetActive(false);
+                        seeTheSing = false;
+        
+                    }
+
                     
+
                 }
+
+
             }
+
+         
         }
         else
         {
 
-            signText.gameObject.SetActive(false);
+            pressE.gameObject.SetActive(false);
         }
     }
 
